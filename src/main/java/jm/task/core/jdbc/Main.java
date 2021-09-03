42d6340b2ccb578554b007c048ffa593eece3af2
Aleksandr Martikhin <a.martikhin@mail.ru>
2021-09-04 00:28:02 +0300

    Codestyle+PrepStatement = resultReview

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,17 +6,13 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     private static final UserService userService = new UserServiceImpl();
-    private static final User USER1 = new User("Anton", "Dubovtcev", (byte) 31);
-    private static final User USER2 = new User("Marat", "Bylallov", (byte) 29);
-    private static final User USER3 = new User("Sergey", "Kislyakov", (byte) 35);
-    private static final User USER4 = new User("Volodya", "Triodin", (byte) 40);
 
     public static void main(String[] args) {
         userService.createUsersTable();
-        userService.saveUser(USER1.getName(), USER1.getLastName(), USER1.getAge());
-        userService.saveUser(USER2.getName(), USER2.getLastName(), USER2.getAge());
-        userService.saveUser(USER3.getName(), USER3.getLastName(), USER3.getAge());
-        userService.saveUser(USER4.getName(), USER4.getLastName(), USER4.getAge());
+        userService.saveUser("Anton", "Dubovtcev", (byte) 31);
+        userService.saveUser("Marat", "Bylallov", (byte) 29);
+        userService.saveUser("Sergey", "Kislyakov", (byte) 35);
+        userService.saveUser("Volodya", "Triodin", (byte) 40);
         userService.getAllUsers();
         userService.cleanUsersTable();
         userService.dropUsersTable();
